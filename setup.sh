48d0ccf0adca0f7c00eba9beaefdd7594d426d81
FaizaS <faiza_shahid12@hotmail.com>
2023-12-08 11:14:11 +0000

    week2-project -3- updated setup file

--- setup.sh
+++ setup.sh
@@ -4,13 +4,16 @@
 set -e
 
 # Define Docker image name
-DOCKER_IMAGE="lbg_faiza"
+DOCKER_IMAGE="lbg_python_api"
 
 cleanup() {
     echo "Cleaning up previous build artifacts..."
     sleep 3
 
+    ssh -i ~/.ssh/id_rsa jenkins@10.154.0.3
+
     # Add commands to clean up previous build artifacts
+    docker stop flask-app || echo "flask-app is not running"
     docker rm -f $(docker ps -aq) || true
     docker rmi -f $(docker images) || true
     echo "Cleanup done."
@@ -20,14 +23,21 @@ cleanup() {
 build_docker() {
     echo "Building the Docker image..."
     sleep 3
-    docker build -t $DOCKER_IMAGE .
+    docker build -t faizashahid/$DOCKER_IMAGE -t faizashahid/$DOCKER_IMAGE:v${BUILD_NUMBER} .
+}
+
+push_docker () {
+    echo "Pushing the docker image..."
+    sleep 3
+    docker push faizashahid/$DOCKER_IMAGE
+    docker push faizashahid/$DOCKER_IMAGE:v${BUILD_NUMBER}
 }
 
 # Function to modify the application
 modify_app() {
     echo "Modifying the application..."
     sleep 3
-   export PORT=5001
+    export PORT=5001
     echo "Modifications done. Port is now set to $PORT"
 }
 
@@ -35,7 +45,8 @@ modify_app() {
 run_docker() {
     echo "Running Docker container..."
     sleep 3
-    docker run -d -p 80:$PORT -e PORT=$PORT $DOCKER_IMAGE
+    ssh -i ~/.ssh/id_rsa jenkins@10.154.0.3
+    docker run -d -p 80:$PORT -e PORT=$PORT --name flask-app faizashahid/$DOCKER_IMAGE
 }
 
 # Main script execution
@@ -44,6 +55,7 @@ sleep 3
 cleanup
 build_docker
 modify_app
+push_docker
 build_docker
 run_docker
 
